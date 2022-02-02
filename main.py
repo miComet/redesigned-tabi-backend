5fccc27b5d07d75fa431c6aac43267fde04e0f28
FATESAIKOU <qzsecftbhhhh@gmail.com>
2022-02-02 22:16:19 +0900

    Complete backend v0.1

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 from typing import Optional
 from fastapi import FastAPI
+from lib.dataclasses import Image, Spot, Record
+
+import json
+import random
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {'Hello': 'World'}
+@app.get("/pic", response_model=list[Image])
+def getPic(pic_num: int):
+    with open('example_data/images.json') as src:
+        images = json.load(src)
+
+    res = []
+    for i in range(pic_num):
+        res.append(Image(**random.choice(images)))
+
+    return res
 
 
-@app.get("/users/{user_id}")
-def read_user(user_id: int, q: Optional[str] = None):
-    return {'user_id': user_id, 'q': q}
+@app.get("/spot_recommendation", response_model=list[Spot])
+def getSpotRecommendation(spot_num: Optional[int] = 1):
+    with open('example_data/spots.json') as src:
+        spots = json.load(src)
+
+    res = []
+    for i in range(spot_num):
+        res.append(Spot(**random.choice(spots)))
+
+    return res
+
+
+@app.post("/record")
+def createRecord(record: Record):
+    print("Save record: " + str(record))
